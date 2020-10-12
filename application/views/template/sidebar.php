9adefbfb26a647c28d56a9aabd29763053bc9e08
Khairuelkahfie <khairoelkahfie@gmail.com>
2020-10-12 08:13:14 +0700

    membuat menu peta

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -24,20 +24,11 @@
                     <span>Dashboard</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-universal-access"></i>
-                    <span>Visual Data</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a>
-                    </div>
-                </div>
+                <a class="nav-link" href="<?= base_url('admin/peta') ?>">
+                <i class="fas fa-map-marker-alt"></i>
+                    <span>Peta Persebaran</span></a>
             </li>
+           
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#master" aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-download"></i>
